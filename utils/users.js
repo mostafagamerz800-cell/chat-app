699ab447d951f5f47be9bcf0fab182189bb0b070
mostafagamerz800-cell <mostafagamerz800@gmail.com>
2026-04-10 15:53:31 +0300

    Create users.js

---
+++ utils/users.js
@@ -0,0 +1,43 @@
+const { duration } = require("moment")
+
+const users = []
+
+
+//jonin user to the chat
+function userjoin(id, username, room) {
+    const user = {id, username, room};
+
+    users.push(user)
+
+    return user;
+}
+
+
+//get the current uesr
+function getcurrentuser(id)  {
+    return users.find(user => user.id === id);
+}
+
+
+//user leave the chat
+function userleave(id) {
+    const index = users.findIndex(user => user.id === id)
+
+if(index !== -1) {
+    return users.splice(index, 1[0])
+ }
+}
+
+
+//get room users
+function getroomusers(room) {
+    return users,filter(user => user.room === room)
+}
+
+
+module.exports = {
+    userjoin,
+    getcurrentuser,
+    userleave,
+    getroomusers
+}
